ff9bf19bd7b92b9b02cd544f8aba2e737ee1c6a2
hussein18149 <husseinkatana17@gmail.com>
2019-06-14 11:22:01 +0300

    generate two class ..for the user new instance and the user credetials

---
+++ credetial.py
@@ -0,0 +1,17 @@
+class user:
+    """
+    class to generate new instance of the user
+    """
+    
+    
+    
+    
+    
+    
+    
+    
+    
+class credetials:
+    """
+    class to create user credetials
+    """
