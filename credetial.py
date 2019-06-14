24360c1a556c9f420a0892f0f60a3002ac5e598c
hussein18149 <husseinkatana17@gmail.com>
2019-06-14 11:29:22 +0300

    creat an empty array to collect user details and credetials

--- credetial.py
+++ credetial.py
@@ -1,7 +1,7 @@
 class user:
-    """
-    class to generate new instance of the user
-    """
+    #class to generate new instance of the user
+    user_list = []
+    user_credetial_list = []
     
     def _init_(self,first_name,second_name,password):
         
