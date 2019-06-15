78c583f44f069e236755dfe2cedebed862030f26
hussein18149 <husseinkatana17@gmail.com>
2019-06-15 12:26:17 +0300

    added changes to the run.py

--- run.py
+++ run.py
@@ -18,3 +18,14 @@ def save_user_credetiaals(user_credetials):
     user_credetials.save_user_credetils()
 def delete_user_credetials(user_credetials):
     user_credetials.delete_user_credetials()
+# def confirm_user(email,password)
+#     confirm_user = User.valid_user(email,password)
+#     return confirm_user
+def del_user_credetials(user_credetials):
+    user_credetials.delete_user_credetials()
+def find_user_credetials(secret_key):
+    return Credetial.find_by_secret_key(secret_key)
+def check_existing_user_create_user_credetials(secret_key):
+    return Credetial.user_credetials_exist(secret_key)
+def display_secret_key():
+    return Credetial.display_user_credetials
