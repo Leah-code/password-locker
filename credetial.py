50edc2b272d6bb78e8d83b2b829cc90ea83e5403
hussein18149 <husseinkatana17@gmail.com>
2019-06-16 13:44:52 +0300

    try to fix the saving functionality

--- credetial.py
+++ credetial.py
@@ -1,29 +1,26 @@
 import pyperclip
 
 class User:
-    #class to generate new instance of the user
+    #class to generate new instance of the users
     user_list = []
-    credetial_list = []
+    Owner_credetial_list = []
+    
+    @classmethod
+    def confirm_users(cls,email,password):
+        old_user = ""
+        for user in User.user_list:
+               if user.email==email and  user.password == password:
+                 old_user = user.email
+        return old_user
     
     def __init__(self,first_name,second_name,user_name,email,password):
         self.first_name = first_name
         self.second_name = second_name
         self.user_name = user_name
         self.email = email
-        self.password = password
+        self.password = password    
     
-    @classmethod
-    def confirm_users(cls,locker_user,password):
-        old_user = ""
-        for user in User.user_list:
-               if user.locker_user and user.password == password:
-                 old_user = user.locker_user
-        return old_user
-    
-        pass
-    
-    
-    def save_user(self):
+    def save_user(self): #saving objects of the user to the list
         User.user_list.append(self)
     
      
@@ -33,34 +30,39 @@ class User:
         
 class Credetial:
     credetials_list = []
-    def __init__(self,Accountname,loginname,mypassword,secretkey):
+    def __init__(self,Accountname,loginname,accountpasssword):
+        
         #docstring for simplicity.....
+        
         self.Accountname = Accountname
         self.loginname = loginname
-        self.mypassword = mypassword
-        self.secretkey = secretkey
+        self.accountpassword = accountpassword
     
     
     def save_user_credetials(self):
         Credetial.credetials_list.append(self)
         
-    def delete_user_credetials(self):
+    def delete_credetials(self):
         Credetial.credetials_list.remove(self)
         
+    # def display_credetials(self):
+    #     Credetial.credetials_list.display(self)
+        
         
 @classmethod
-def find_by_secretkey(cls,secretkey):
-    for credetials in cls.secretkey  == secretkey:
-        return credetials
+def find_by_Accountname(cls,Accountname):
+    for credetials in cls.credetial_list:
+        if credetials.Accountname == Accountname:
+            return credetials
 
 @classmethod
-def user_credetials_exists(cls,Accountname):
-        for account in cls.credetials_list:
-            if account.secretkey == secretkey:
+def credetials_exists(cls,Accountname):
+        for Accountname in cls.credetials_list:
+            if Accountname.Accountname == "Accountname":
                 return True
             else:
                 return False
 
 @classmethod
-def display_user_credetials(cls):
+def display_credetials(cls):
     return cls.credetials_list
--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3.6
-
-from credetial import User, Credetial, display_user_credetials
+from credetial import User, Credetial
 # import random
 # import getpass
 
@@ -9,15 +8,20 @@ from credetial import User, Credetial, display_user_credetials
 def create_user(first_name,second_name,user_name,email,password):
     new_user = User(first_name,second_name,user_name,email,password)
     return new_user
+
 def save_user(user):
     user.save_user()
+    
 def del_user(user):
     user.delete_user()
-def create_user_credetials(Accountname,loginname,mypassword,secretkey):
-    new_user_credetials = Credetial(Accountname,loginname,mypassword,secretkey)
+    
+def create_user_credetials(Accountname,loginname,accountpassword,secretkey):
+    new_user_credetials = Credetial(Accountname,loginname,accountpassword,secretkey)
     return new_user_credetials
+
 def save_user_credetiaals(user_credetials):
     user_credetials.save_user_credetils()
+    
 def delete_user_credetials(user_credetials):
     user_credetials.delete_user_credetials()
 # def confirm_user(email,password)
@@ -25,12 +29,16 @@ def delete_user_credetials(user_credetials):
 #     return confirm_user
 def del_user_credetials(user_credetials):
     user_credetials.delete_user_credetials()
+    
 def find_user_credetials(secretkey):
     return Credetial.find_by_secretkey(secretkey)
+
 def check_existing_user_create_user_credetials(secretkey):
     return Credetial.user_credetials_exist(secretkey)
-def display_secret_key():
+
+def display_secretkey():
     return Credetial.display_user_credetials
+
 def display_user_credetials():
     return Credetial.display.user_credetials()
 
@@ -49,39 +57,12 @@ def main():
                         Accountname = input()
                         print("login_name")
                         loginname = input()
-                        print ("mypassword")
+                        print ("accountpassword")
                         accountpassword = input()
                         print("secretkey")
-                        secret_key = input()  
-                        
-                        save_user_credetiaals(Credetial(Accountname,loginname,mypassord,secretkey))
-                        print(f'here are your account details: \n......{Accountname}\n........{loginname}\n..........{mypassword}.....{secretkey}')
-                        # save_credetials(save_user_credetials(Account_name, login_name, acc_password, secret_key))
-                    # print(f"new_user_credetials{Account_name} {login_name} {acc_password} {secret_key} created")
-                # elif short_code == 'see':
-                #         if display_user_credetials():
-                #             print("HERE IS A LIST OF YOUR PASSWORDS FOR DIFFERENT ACCOUNT")
-                #             print("/n")
-                #             for credetials in display_user_credetials():
-                #                 print(f"{user_credetials.Account_name} {user_credetials.login_name} {user_credetials.acc_password}  {user_credetials.secret_key}")
-                #                 print("/n")
-                # elif short_code == 'look':
-                #         print("ENTER THE SECRET_KEY YOU WANT TO FIND")
-                #         secret_key = input()
-                #         if check_existing_user_credetials(secret_key):
-                #             search_user_credetials = find_user_credetials(secret_key)
-                #             print(f"{search_user_credetials.Account_name} {search_user_credetials.login_name}  {search_user_credetials.acc_password} {search_user_credetials.secret_key}")
-                #             print('_' * 25)
-                #             print(f"Account-name....{search_user_credetials.Account_name}")
-                #             print(f"Login-name....{search_user_credetials.login_name}")
-                #             print(f"Password....{search_user_credetials.acc_password}")
-                #             print(f"Unique-key....{search_user_credetials.secret_key}")
-                #         else:
-                #             print("THAT ACCOUNT IS NOT RECOGONISED")
-                # elif short_code == "quit":
-                #     print("SEE YOU NEXT TIME DEAR FRIEND")
-                # else:
-                #     print("WRONG CHOICE....")
-                
+                        secret_key = input()                         
+                        save_user_credetiaals(Credetial(Accountname,loginname, accountpassword, secret_key))
+                        print(f'here are your account details: .{Accountname}..{loginname}...{accountpassword}.....{secret_key}')
+                                       
 if __name__ == '__main__':
     main()
